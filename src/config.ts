182a0f7238ceb84dda80e6755ba4bb024d50325e
HdA <gaborneubauer.hda@gmail.com>
2023-05-29 18:55:03 +0200

    UPDATE config.ts with deployed frontend's callback url

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 // Google login URL
 const url = "https://accounts.google.com/o/oauth2/v2/auth"
 const client_id = "231875441502-e9o5b1ivucareb2r4bplgqpf7326cpe7.apps.googleusercontent.com"
-const redirect_URI = "http://localhost:5173/callback"
+// const redirect_URI = "http://localhost:5173/callback"       // When running locally
+const redirect_URI = "https://landlord-app.netlify.app/about/callback"
 const scope = "profile%20email%20openid"
 const response_type = "code"
 
@@ -9,9 +10,11 @@ export const fullUrl = `${url}?client_id=${client_id}&redirect_uri=${redirect_UR
 
 // Landlord backend server URL
 export const landlordBackendUrl = "https://landlord-backend-dvw43.ondigitalocean.app"
+// export const landlordBackendUrl = "http://localhost:3003"       // When running locally
 
 // OpenApi (Swagger) 3.0 documentation URL
 export const landlordApiDocsUrl = "https://landlord-backend-dvw43.ondigitalocean.app/api/docs"
+// export const landlordApiDocsUrl = "http://localhost:3003/api/docs"       // When running locally
 
 // Backend readme.md GitHub URL
 export const landlordBackendReadmeUrl = "https://github.com/HordodongA/Codecool-Full-Stack-Exam-Project_Backend/blob/main/readme.md"
